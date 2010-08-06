5d5000d08136e09719e0ec5a984565f1833b6182
Noah Kantrowitz <noah@coderanger.net>
2010-08-06 15:49:39 -0700

    Totally untested Linux UI using pynotify (which might be deprecated).

---
+++ pyzen/ui/linux.py
@@ -0,0 +1,24 @@
+from pyzen.ui.base import img_path, PyZenUI
+
+try:
+    import pynotify
+except ImportError:
+    pynotify = None
+
+class LibnotifyUI(PyZenUI):
+    """A PyZen UI that uses libnotify (via pynotify)."""
+    
+    name = 'libnotify'
+    platform = 'linux2'
+
+    def __init__(self):
+        self.has_notify = pynotify.init('PyZen')
+    
+    @classmethod
+    def enabled(cls):
+        return super(LibnotifyUI, cls).enabled() and pynotify is not None
+    
+    def notify(self, failure, title, msg, icon):
+        if self.has_notify:
+            n = pynotify.Notification(title, msg, 'file://'+img_path(icon))
+            n.show()
