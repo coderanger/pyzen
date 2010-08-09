b7f7c50993f89da32656ac822bd11566934dc66a
Noah Kantrowitz <noah@coderanger.net>
2010-08-09 01:58:04 -0700

    Functional ``setup.py zen`` command that uses the setuptools loader system. This code hasn't changed at all in distribute, so I suspect it should work there too.

--- pyzen/distutils.py
+++ pyzen/distutils.py
@@ -1,15 +1,20 @@
+import sys
+import unittest
+
 from setuptools.command.test import test
 from pkg_resources import *
+
+from pyzen.core import main
+
+def run_tests(loader, args, path):
+    sys.path[:] = path
+    loader_ep = EntryPoint.parse("x="+loader)
+    loader_class = loader_ep.load(require=False)
+    unittest.main(None, None, [unittest.__file__]+args, testLoader=loader_class())
 
 class zen(test):
     """Command to run test suite under PyZen."""
     description = 'run unit tests under PyZen'
 
     def run_tests(self):
-        import unittest
-        loader_ep = EntryPoint.parse("x="+self.test_loader)
-        loader_class = loader_ep.load(require=False)
-        unittest.main(
-            None, None, [unittest.__file__]+self.test_args,
-            testLoader = loader_class()
-        )
+        main(None, run_tests, self.test_loader, self.test_args, sys.path[:])
