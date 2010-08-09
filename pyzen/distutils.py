c609fd1141e9d522a76d0b8fb93b2dc1acb7fe29
Noah Kantrowitz <noah@coderanger.net>
2010-08-09 01:49:01 -0700

    Copy the main runner function from the test command so I can play with it.

--- pyzen/distutils.py
+++ pyzen/distutils.py
@@ -1,9 +1,15 @@
 from setuptools.command.test import test
+from pkg_resources import *
 
 class zen(test):
     """Command to run test suite under PyZen."""
     description = 'run unit tests under PyZen'
 
-    def run(self):
-        """runner"""
-        print 'PyZen'
+    def run_tests(self):
+        import unittest
+        loader_ep = EntryPoint.parse("x="+self.test_loader)
+        loader_class = loader_ep.load(require=False)
+        unittest.main(
+            None, None, [unittest.__file__]+self.test_args,
+            testLoader = loader_class()
+        )
