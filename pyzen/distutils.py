4807909867a55cafd9b118afca296bece1bfc6c2
Noah Kantrowitz <noah@coderanger.net>
2010-08-09 03:20:20 -0700

    Rework the distuils runner to get the real results. Basically a mini-backport of the exit=False feature from 2.7.

--- pyzen/distutils.py
+++ pyzen/distutils.py
@@ -1,28 +1,35 @@
 import sys
 import unittest
+import types
 
 from setuptools.command.test import test
 from pkg_resources import *
 
 from pyzen.core import main
 
+class ZenTestProgram(unittest.TestProgram):
+    
+    def runTests(self):
+        if self.testRunner is None:
+            self.testRunner = TextTestRunner
+
+        if isinstance(self.testRunner, (type, types.ClassType)):
+            try:
+                testRunner = self.testRunner(verbosity=self.verbosity)
+            except TypeError:
+                # didn't accept the verbosity argument
+                testRunner = self.testRunner()
+        else:
+            # it is assumed to be a TestRunner instance
+            testRunner = self.testRunner
+        self.result = testRunner.run(self.test)
+
 def run_tests(loader, args, path):
     sys.path[:] = path
     loader_ep = EntryPoint.parse("x="+loader)
     loader_class = loader_ep.load(require=False)
-    fail = False
-    try:
-        unittest.main(None, None, [unittest.__file__]+args, testLoader=loader_class())
-    except SystemExit, e:
-        if e.code:
-            fail = True
-    
-    result = unittest.TestResult()
-    result.total = -1
-    # len(failures)==1 indicates failure
-    if fail:
-        result.failures.append(None)
-    return result
+    m = ZenTestProgram(None, None, [unittest.__file__]+args, testLoader=loader_class())
+    return m.result
 
 class zen(test):
     """Command to run test suite under PyZen."""
